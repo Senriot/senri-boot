77be9acf45a880e0c579e27dfa2a0f35da1f1d31
Allen <j316920@gmail.com>
2021-08-01 22:01:10 +0800

    Initial commit

---
+++ src/main/kotlin/com/senriot/boot/Hello.kt
@@ -0,0 +1,6 @@
+package com.senriot.boot
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/com/senriot/boot/HelloTest.kt
@@ -0,0 +1,8 @@
+package com.senriot.boot
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
